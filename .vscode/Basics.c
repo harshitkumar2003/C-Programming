06858eb91cb2707728650bd4ce5d4ce3ebf827b0
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-03-25 01:29:23 +0530

    `Update Basics.c with additional comments and formatting`

--- .vscode/Basics.c
+++ .vscode/Basics.c
@@ -1,8 +1,13 @@
-// most basic hello world program 
-#include<stdio.h>
-int main()
+// C is a foundational programming language 
+// Lets start with basics
+
+// --------------------------------------------------
+// Part 1 Basics (Variables, Operators, Input/Output)
+// --------------------------------------------------
+#include<stdio.h>   // header file / preprocessor directives 
+int main()  // main fucntion in a c program atleast one function will be present
 {
-    printf("hello world");
+    printf("hello world");  // displaying hello world
     return 0;
 }
 
