84dec2767bcdedc7151e2e8f21887db945e004d7
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-03-25 01:52:19 +0530

    `Added comments to Basics.c explaining basic C concepts`

--- .vscode/Basics.c
+++ .vscode/Basics.c
@@ -7,11 +7,23 @@
 #include<stdio.h>   // header file / preprocessor directives 
 int main()  // main fucntion in a c program atleast one function will be present
 {
-    printf("hello world");  // displaying hello world    
-    printf("\n\t"); // Escape Sequence (\n for new line), (\t for spacing 4 blocks)
+    // variable is where we store a value (a is a variable)
+    // operators is mathematical task to be assign for a result ( = is a operator 10 is assign to a) 
+    int a=10; 
+    // displaying hello world (printf is used for display message or output)  
+    printf("hello world");  
+    // Escape Sequence 
+    // (\n for new line)
+    // (\t for spacing 4 blocks)
+    printf("\n\t");
     printf("hello Harshit"); 
- 
-    return 0;
+    //  we can perform calculation while displaying (addition of 10+10)
+    //  %d is a format specifier for displaying integer
+    //  %c is a format specifier for displaying character 
+    //  %s is a format specifier for displaying string
+    //  %f is a format specifier for displaying floating number
+    printf("\n%d",a+a);
+    return 0;  // end a program
 }
 
 
