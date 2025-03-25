96089af3400395ef509db9e6ad8de505f1b43057
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-03-25 17:55:35 +0530

    `Added comments and arithmetic operations to Basics.c`

--- .vscode/Basics.c
+++ .vscode/Basics.c
@@ -7,22 +7,37 @@
 #include<stdio.h>   // header file / preprocessor directives 
 int main()  // main fucntion in a c program atleast one function will be present
 {
-    // variable is where we store a value (a is a variable)
-    // operators is mathematical task to be assign for a result ( = is a operator 10 is assign to a) 
+    // variable is where we store a value (a and b is a variable)
+    // operators is mathematical task to be assign for a result ( =,+,-,*,/ is a operator 10 is assign to a) 
     int a=10; 
+    int b=3;
+
     // displaying hello world (printf is used for display message or output)  
     printf("hello world");  
-    // Escape Sequence 
+
+    // Use of escape sequence 
     // (\n for new line)
     // (\t for spacing 4 blocks)
-    printf("\n\t");
-    printf("hello Harshit"); 
-    //  we can perform calculation while displaying (addition of 10+10)
+    printf("\nhello Harshit"); 
+    printf("\thello user\n"); 
+
+    // for displaying values of a variable
     //  %d is a format specifier for displaying integer
     //  %c is a format specifier for displaying character 
     //  %s is a format specifier for displaying string
     //  %f is a format specifier for displaying floating number
-    printf("\n%d",a+a);
+    printf("%d",a);
+    printf("\n%d",b);
+
+    // Aruthmetic operation using operation
+    // addition
+    printf("\n%d",a+b);
+    // subtraction
+    printf("\n%d",a-b);
+    // multiplication
+    printf("\n%d",a*b);
+    // division
+    printf("\n%d",a/b);
     return 0;  // end a program
 }
 
