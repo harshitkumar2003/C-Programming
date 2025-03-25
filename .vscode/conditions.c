2478e88fa4a945853154f49384e1560891935e12
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-03-26 01:46:22 +0530

    `Added cases 2-5 to switch statement and removed commented out cases`

--- .vscode/conditions.c
+++ .vscode/conditions.c
@@ -94,19 +94,19 @@ int main()
 //     case 1:
 //         printf("a is 1");
 //         break;
-//     case 2:
-//         printf("a is 2");
-//         break;
-//     case 3:
-//         printf("a is 3");
-//         break;
-//     case 4:
-//         printf("a is 4");
-//         break;
-//     case 5:
-//         printf("a is 5");
-//         break;
-//     default:
-//         printf("a is not 1,2,3,4,5");
-//         break;
-// }
+     case 2:
+         printf("a is 2");
+         break;
+     case 3:
+         printf("a is 3");
+         break;
+     case 4:
+         printf("a is 4");
+         break;
+     case 5:
+         printf("a is 5");
+         break;
+     default:
+         printf("a is not 1,2,3,4,5");
+         break;
+ }
