0ea110852be245ab8650f18073c7cc282b82424c
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-03-25 21:27:48 +0530

    `Added explanations and examples for if, if-else, if-else if-else, and nested if statements in conditions.c`

--- .vscode/conditions.c
+++ .vscode/conditions.c
@@ -10,16 +10,52 @@ int main()
     printf("Enter the third number: ");
     scanf("%d",&c);// taking value from the user
 
+// -----------------
     // condition 
-    // if conditions allow you to execute certain blocks of code based on whether a specified condition is true or false
-    for example 
-    int a = 5;
-    if (a > 0) {
-        printf("a is positive\n");
-    }
+// -----------------
+    // 1. Basic if statement: if conditions allow you to execute certain blocks of code based on whether a specified condition is true or false
+    // for example 
+    // int a = 5;
+    // if (a > 0) {
+    //     printf("a is positive\n");
+    // }
+
+// -----------------------------------------------------------------------------------------------
+    // 2. if-else statement: An if statement can be followed by an else statement, which is executed if the condition is false.
+    // for example
+    // int a = 5;
+    // if (a > 0) {
+    //     printf("a is positive\n");
+    // } else {
+    //     printf("a is not positive\n");
+    // }
+
+// -----------------------------------------------------------------------------------------------
+    // 3. if-else if-else chain: multiple if conditions with else if to check more than one condition in sequence.
+    // for example 
+    // int a = 0;
+    // if (a > 0) {
+    //     printf("a is positive\n");
+    // } else if (a == 0) {
+    //     printf("a is zero\n");
+    // } else {
+    //     printf("a is negative\n");
+    // }
+
+// ------------------------------------------------------------------------------------------------
+    // 4. Nested if statements: if statements inside other if statements, which is known as nesting.
+    // for example 
+    // int a = 5;
+    // int b = 10;
+    // if (a > 0) {
+    //     if (b > 5) {
+    //  printf("a is positive and b is greater than 5\n");
+    //   }
+    //}
 
 
-    // if else if else
+// ------------------------------------------------------------------------------------------------
+
 
     // checking largest values using if else condition
     if(a>b && a>c)  // if a greater than b and greater than c
