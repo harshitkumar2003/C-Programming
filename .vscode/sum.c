be8539a73327899e25d84c60028703dcf3dbb8a9
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-03-30 22:56:29 +0530

    `Removed commented out code in sum.c`

--- .vscode/sum.c
+++ .vscode/sum.c
@@ -1,12 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,c;
-    printf("Enter the value of a:");
-    scanf("%d",&a);
-    printf("Enter the value of b:");
-    scanf("%d",&b);
-    c=a+b;
+    // int a,b,c;
+    // printf("Enter the value of a:");
+    // scanf("%d",&a);
+    // printf("Enter the value of b:");
+    // scanf("%d",&b);
+    // c=a+b;
+    // printf("The sum of a and b is: %d",c);
     printf("The sum of a and b is: %d",c);
 return 0;
 }
