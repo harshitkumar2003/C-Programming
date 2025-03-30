f8c418cc87b6c818dacd305679a88ba7e3bf0324
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-03-30 23:01:58 +0530

    `Added comments to hello.c`

--- .vscode/hello.c
+++ .vscode/hello.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
 int main()
 {
+    // -------------------------------------------------------------
     // Printing hello world the basic first program
     printf("Hello World");
+    // -------------------------------------------------------------
+
+    // -------------------------------------------------------------
     // int a,b,c;
     // printf("Enter the value of a:");
     // scanf("%d",&a);
@@ -10,5 +14,6 @@ int main()
     // scanf("%d",&b);
     // c=a+b;
     // printf("The sum of a and b is: %d",c);
+    // -------------------------------------------------------------
 return 0;
 }
