55d25c1f899ac40e31351686839f5bad8a428867
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-03-30 23:11:37 +0530

    update and add new file

---
+++ .vscode/keywords.c
@@ -0,0 +1,41 @@
+// =====================
+// keywords in c language
+
+// ---------------------------------------------------------
+//  there are 32 keywords in c programming language
+// 1. auto
+// 2. break
+// 3. case
+// 4. char
+// 5. const
+// 6. continue
+// 7. default
+// 8. do
+// 9. double
+// 10. else
+// 11. enum
+// 12. extern
+// 13. float
+// 14. for
+// 15. goto
+// 16. if
+// 17. int
+// 18. long
+// 19. register
+// 20. return
+// 21. short
+// 22. signed
+// 23. static
+// 24. struct
+// 25. switch
+// 26. typedef
+// 27. union
+// 28. unsigned
+// 29. void
+// 30. volatile
+// 31. while
+// 32. _Bool
+// ---------------------------------------------------------
+
+// --------------------------------------------------------
+
