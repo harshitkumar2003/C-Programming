6182d1700a08310c4db4e335b10900664c007963
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-03-31 13:05:06 +0530

    `git commit -m "Updated hello.c with unchanged code block"`

--- .vscode/hello.c
+++ .vscode/hello.c
@@ -8,12 +8,12 @@ int main()
 
     // -------------------------------------------------------------
     // Printing sum of two variable 
-     int a,b;
-     printf("Enter the value of a:");
-     scanf("%d",&a);
-     printf("Enter the value of b:");
-     scanf("%d",&b);
-     printf("The sum of a and b is: %d", a+b);
+    int a,b;
+    printf("Enter the value of a:");
+    scanf("%d",&a);
+    printf("Enter the value of b:");
+    scanf("%d",&b);
+    printf("The sum of a and b is: %d", a+b);
     // -------------------------------------------------------------
 return 0;
 }
