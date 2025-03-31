58b353235488a42cbe59c8f9d9120cad9410486b
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-03-31 23:28:14 +0530

    `Updated program1.c with multiple C programs and added new homework questions`

--- .vscode/program1.c
+++ .vscode/program1.c
@@ -1,14 +1,92 @@
+// -------------------------------------------------------------------------------------------------
+// WAP to check the number is divisible 2 or not
+
+// #include<stdio.h>
+// int main ()
+// {
+//     int num;
+//     printf("Enter a number: ");
+//     scanf("%d", &num);
+//     if(num % 2 == 0)   (if the number of a remainder will be zero it will divisible by 2)
+//     {
+//          printf("the number is divisible by 2");
+//     }
+//     else
+//     printf("the number is not divisible by 2");
+//     return 0;
+// }
+// -------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------
+// WAP to check if 1(true) or 0(false) for following statement:
+// if it's sunday and it's snowing = true
+// if it's monday or it's raining = true
+// if a number is greater than 9 and less than 100 = true
+
+//  #include<stdio.h>
+//  int main ()
+//  {
+//      int sunday = 1;
+//      int snowing = 1;
+//      if(sunday && snowing == 1)
+//      printf("true");
+//     else
+//     printf("false");
+//     return 0;
+//  }
+//  -------------------------------------------------------------------------------------------------
+
+//  #include<stdio.h>
+//  int main ()
+//  {
+//      int monday = 1;
+//      int raining = 0;
+//      if(monday || raining == 1)
+//      printf("true");
+//     else
+//     printf("false");
+//     return 0;
+//  }
+//  ------------------------------------------------------------------------------------------------
+
+//  #include<stdio.h>
+//  int main ()
+//  {
+//      int num;
+//      printf("Enter a number: ");
+//      scanf("%d", &num);
+//      if(num > 9 && num < 100)
+//      printf("true");
+//     else
+//     printf("false");
+//     return 0;
+//  }
+// ------------------------------------------------------------------------------------------------
+
+
+// ------------------------------------------------------------------------------------------------
+// Homework Question
+// 1. WAP to print the average of 3 numbers
+
 #include<stdio.h>
-int main ()
-{
-    int num;
-    printf("Enter a number: ");
-    scanf("%d", &num);
-    if(num % 2 == 0)
-    {
-         printf("the number is divisible by 2");
-    }
-    else
-    printf("the number is not divisible by 2");
+ int main ()
+ {
+   float a,b,c;
+   float avg;
+     printf("Enter a number: ");
+     scanf("%f", &a);
+     printf("Enter a number: ");
+     scanf("%f", &b);
+     printf("Enter a number: ");
+     scanf("%f", &c);
+     avg = (a+b+c) / 3;
+     printf("the average of 3 number is: %f",avg);
     return 0;
-}
+ }
+//  ----------------------------------------------------------------------------------------------
+// 2. WAP to check if given character is digit or not
+
+//  ----------------------------------------------------------------------------------------------
+//  ----------------------------------------------------------------------------------------------
+
+// 3. WAP to print the smallest number
