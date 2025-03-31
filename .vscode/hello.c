e87c6223c929e09d1dc096ed249bd8740b4dc48e
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-03-31 13:58:51 +0530

    `Refactor hello.c: modify printf statements, add variable declarations and arithmetic operations`

--- .vscode/hello.c
+++ .vscode/hello.c
@@ -3,17 +3,22 @@ int main()
 {
     // -------------------------------------------------------------
     // Printing hello world the basic first program
-    printf("Hello World");
+    // printf("Hello World");
     // -------------------------------------------------------------
 
     // -------------------------------------------------------------
     // Printing sum of two variable 
-    int a,b;
-    printf("Enter the value of a:");
-    scanf("%d",&a);
-    printf("Enter the value of b:");
-    scanf("%d",&b);
-    printf("The sum of a and b is: %d", a+b);
+    int a = 2;
+    int b = 1;
+    // printf("Enter the value of a:");
+    // scanf("%d",&a);
+    // printf("Enter the value of b:");
+    // scanf("%d",&b);
+    printf("\nThe sum of a and b is: %d", a+b);
+    printf("\nThe product of a and b is: %d", a-b);
+    printf("\nThe multiolication of a and b is: %d", a*b);
+    printf("\nThe divisor of a and b is: %d", a/b);
+    printf("\nThe remainder of a and b is: %d", a%b);
     // -------------------------------------------------------------
 return 0;
 }
