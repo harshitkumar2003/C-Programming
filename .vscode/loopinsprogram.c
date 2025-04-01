9126ba6a057de206eb7b88fb078a84ae92f9163e
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-04-01 22:50:32 +0530

    "Deleted .vscode/hello.exe file"

---
+++ .vscode/loopinsprogram.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+  int main ()
+  {
+      int num, i;
+      printf("Enter a num: ");
+      scanf("%d", &num);
+      for(i = 1; i <= num; i++)
+      {
+           printf("hello world\n");
+      }
+     return 0;
+  }
