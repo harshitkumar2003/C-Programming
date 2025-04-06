60778ebc527f88e5693a6654fdfc81e2f4522258
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-04-06 23:24:14 +0530

    `Update loopinsprogram.c: modify for loop range and add newline at end of file`

--- .vscode/loopinsprogram.c
+++ .vscode/loopinsprogram.c
@@ -34,12 +34,13 @@
     // }
 
 // -----------------------------------------------------------------------
-// #include<stdio.h>
-//   int main ()
-//   {
-//       for(int i = 0; i <= 10; i++)
-//       {
-//            printf("%d\n",i);
-//       }
-//      return 0;
-//   }
+ #include<stdio.h>
+   int main ()
+   {
+       for(int i = 1; i <= 10; i++)
+       {
+            printf("%d\n",i);
+       }
+      return 0;
+   }
+
