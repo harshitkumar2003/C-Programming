5f76e2cadb531b748c639b4b7d9e9fe85bb34688
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-04-07 12:35:03 +0530

    `Refactored code in loopinsprogram.c and updated executable`

--- .vscode/loopinsprogram.c
+++ .vscode/loopinsprogram.c
@@ -38,11 +38,26 @@
  #include<stdio.h>
    int main ()
    {
-    int i,j;
-       for(i = 0,j = i; i <= 10 && j <= i; i++,j--)
-       {
-            printf("%d\n",j);
-       }
-      return 0;
+    char name[3];
+    char temp[3];
+    printf("Program for entering names in an array of three:\n ");
+    for(int i = 1; i <= 3; i++){
+        printf("Enter name %d: ", i);
+        scanf("%s", name);
+    }
+    temp[3] = name[3];
+    for(int j = 0; j <= 3; j++){
+        printf("Name %d: %c\n", j, temp);
+    }
+    return 0;
    }
+//    {
+//     int array
+//     int i,j;
+//        for(i = 0,j = i; i <= 10 && j <= i; i++,j--)
+//        {
+//             printf("%d\n",j);
+//        }
+//       return 0;
+//    }
 
