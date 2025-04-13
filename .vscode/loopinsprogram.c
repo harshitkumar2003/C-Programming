a697f8a56e46c4f671033efefc147d7eae67003f
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-04-14 00:40:09 +0530

    `Updated loopinsprogram.c to print odd even numbers up to 10 using do while loop`

--- .vscode/loopinsprogram.c
+++ .vscode/loopinsprogram.c
@@ -81,14 +81,18 @@
     // -----------------------------------------------------------------------
 
     // -----------------------------------------------------------------------
-    // Using do while loop print odd even numbers 
+    // Using do while loop print odd even numbers upto 10
     #include<stdio.h>
     int main()
     {
         int a=1;
         while(a<=10){
-            printf("Enter a number: "); scanf("%d", &a);
-        }
-    return 0;
-    }
+            if(a%2==0)
+            printf("%d is even\n",a);
+        else
+        printf("%d is odd\n",a);
+    a++;
+}
+return 0;
+}
     // -----------------------------------------------------------------------
