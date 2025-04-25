d93485e71ed34e680b1866d51d8f05f21d269cb3
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-04-25 23:57:28 +0530

    "Added a second bullet point to the comment explaining pointers in .vscode/pointers.c"

--- .vscode/pointers.c
+++ .vscode/pointers.c
@@ -5,3 +5,4 @@
 // Pointers are variables that store the address of another variable. They are used to manipulate data in memory directly and can be used to create dynamic data structures like linked lists, trees, etc.
 // ------------------------------------------------------------------------------------------------------------
 // * 1. Pointers are variables that store memory addresses of other variables.
+// * 2. They are declared using the asterisk (*) symbol before the variable name.
