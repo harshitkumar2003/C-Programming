30bd9803137c787c56d6bc19e7c887583403c0f5
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-04-26 00:18:25 +0530

    `Added two new points to the list of pointer uses in .vscode/pointers.c`

--- .vscode/pointers.c
+++ .vscode/pointers.c
@@ -19,3 +19,4 @@
 // * 12. Pointers can be used to create and manipulate arrays of arrays and arrays of pointers to functions.
 // * 13. Pointers can be used to create and manipulate arrays of pointers to structures and structures of pointers to arrays.
 // * 14. Pointers can be used to create and manipulate arrays of pointers to functions and functions of pointers to arrays.
+// * 15. Pointers can be used to create and manipulate arrays of pointers to pointers and pointers to pointers to arrays.
