7c5bac41df4821fc9fb6bce5f9be7bc7f9a7ae23
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-04-25 23:57:10 +0530

    `Added additional comments to .vscode/pointers.c`

--- .vscode/pointers.c
+++ .vscode/pointers.c
@@ -3,3 +3,5 @@
 // ======================
 
 // Pointers are variables that store the address of another variable. They are used to manipulate data in memory directly and can be used to create dynamic data structures like linked lists, trees, etc.
+// ------------------------------------------------------------------------------------------------------------
+// * 1. Pointers are variables that store memory addresses of other variables.
