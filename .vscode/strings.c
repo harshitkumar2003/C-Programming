60f47201d3db18ed16cb5a3000c72cd5d53f5cc4
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-05-09 00:17:36 +0530

    `Update strings.c documentation with examples of character array initialization`

--- .vscode/strings.c
+++ .vscode/strings.c
@@ -3,10 +3,12 @@
 // =============================================
 // A character array is a data structure in C that stores a sequence of characters. It is commonly used to represent strings. In C, strings are null-terminated, meaning that the last character in the array is followed by a special null character ('\0') to indicate the end of the string.
 
-// for example, the string "Hello" can be represented as a character array as follows:
+// For example, the string "Hello" can be represented as a character array as follows:
 // char str[6] = "Hello"; // 5 characters + 1 null terminator
 
 // The size of the array must be large enough to accommodate all characters in the string, including the null terminator.
 
 // If the size is not specified, the compiler will automatically allocate enough space for the string and the null terminator.
+// For example:
+// char str[] = "Hello"; // Compiler allocates 6 bytes (5 characters + 1 null terminator)
 
