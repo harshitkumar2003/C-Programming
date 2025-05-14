21a76e59b8e272292e2f3d1595bb7f8cbcf894cc
Harshit kumar <106154167+harshitkumar2003@users.noreply.github.com>
2025-05-14 11:22:39 +0530

    add one important comment for understanding of strings input

--- .vscode/strings.c
+++ .vscode/strings.c
@@ -39,6 +39,7 @@
 
 
 // ----------------------------------------------------------------------------------------------------
+// In read input string is also a pointer don't need to (&) while input
 // In the above code, we declare and initialize a string using both methods. The first method uses double quotes to create a string literal, while the second method uses a character array with individual characters. Both methods are valid in C.
 // The %s format specifier is used to print strings in C. It automatically handles the null terminator, so you don't need to worry about it when printing strings.
 // The output of the above code will be:
