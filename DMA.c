4664a6acd096f9ece9b7a2d966e6e20f581e9139
Harshit Kumar <harshitkumar2k03@gmail.com>
2025-05-19 22:22:30 +0530

    started DMA

---
+++ DMA.c
@@ -0,0 +1,8 @@
+// ==========================================================
+// Dynamic Memory Allocator (DMA) 
+// ==========================================================
+// It is way to allocate and deallocate memory dynamically at runtime.
+// We need some functions to allocate and free memory dynamically.
+// functions for dynamic memory allocation
+calloc
+
